8f65186c02b2e5b590da12052893b78b8476d148
ddonnie <mohax91@gmail.com>
2017-01-31 09:48:50 +0000

    add comment remove

--- blog/models.py
+++ blog/models.py
@@ -26,11 +26,6 @@ class Comment(models.Model):
     author = models.ForeignKey('auth.User')
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    approved_comment = models.BooleanField(default=False)
-
-    def approve(self):
-        self.approved_comment = True
-        self.save()
 
     def __str__(self):
         return self.text
